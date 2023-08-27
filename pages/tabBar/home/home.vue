8c875a797d68e4df738cb0390b500eaf478112c0
a1592 <709787749@qq.com>
2023-08-27 15:15:46 +0800

    feat: phase 1 ends

--- pages/tabBar/home/home.vue
+++ pages/tabBar/home/home.vue
@@ -1,45 +1,44 @@
 <template>
 	<view class="container">
-		<!--header-->
-		<view class="tui-header-banner">
-			<view class="tui-banner-bg">
-				<view class="tui-primary-bg tui-route-left"></view>
-				<view class="tui-primary-bg tui-route-right"></view>
-				<!--banner-->
-				<view class="tui-banner-box">
-					<swiper :indicator-dots="true" :autoplay="true" :interval="5000" :duration="150" class="tui-banner-swiper"
-					 :circular="true" indicator-color="rgba(255, 255, 255, 0.8)" indicator-active-color="#fff">
-						<swiper-item v-for="(item, index) in banner" :key="index">
-							<image :src="'https://thorui.cn/images/mall/banner/' + item" class="tui-slide-image" mode="scaleToFill" />
-						</swiper-item>
-					</swiper>
-				</view>
+		<view class="top-pic">
+			<view class="text1">
+				<text> 豫见山海 </text>
+			</view>
+			<view class="text2">
+				<text> 躬行以践，奔赴山海 </text>
 			</view>
 		</view>
-
-		<view class="tui-product-box">
-			<view class="tui-block__box tui-mtop__20">
-				<view class="tui-new-box">
-					<view class="tui-new-item1" @click="onclick(1)" >
-						<view class="tui-title-box">
-							<view>
-								<h2 > 产业展示I</h2>
-							</view>
-						</view>
-					</view>
-					
-					<view class="tui-new-item2" @click="onclick(2)">
-						<view class="tui-title-box">
-							<view class>
-								<h2> 产业展示II</h2>
-							</view>
-						</view>
-					</view>
-				</view>
+	
+		<view class="info">
+			<view class="info-text">
+				<text> 习近平总书记在党的二十大报告中指出，“发展乡村特色产业，拓宽农民增收致富渠道。巩固拓展脱贫攻坚成果，增强脱贫地区和脱贫群众内生发展动力。”产业振兴助力乡村振兴，仍是建设新时代农业强国的重要课题。 </text>
 			</view>
 		</view>
 		
-
+		<view class="product-box1" @click="onclick(1)">  
+			<view class="text3">
+				<text> 产业成果 I </text>
+			</view>
+			<view class="text4">
+				<text> 成果简介 </text>
+			</view>
+		</view>
+		
+		<view class="product-box2" @click="onclick(1)"> 
+			<view class="text5">
+				<text> 产业成果 II </text>
+			</view>
+		</view>
+		
+		<view class="show-text">
+			<text> 数据展示 </text>
+		</view>
+		
+		<view class="stats1">
+		</view>
+		
+		<view class="stats2">
+		</view>
 	</view>
 </template>
 <script>
@@ -102,608 +101,163 @@
 </script>
 
 <style>
-	page {
-		background-color: #f7f7f7;
-	}
-
-	.container {
-		padding-bottom: 100rpx;
-		color: #333;
-	}
-
-	.tui-header {
+	.top-pic{
+		left: 0px;
+		top: 1px;
 		width: 100%;
-		height: 100rpx;
-		padding: 0 20rpx;
-		box-sizing: border-box;
-		background-color: #aaffff;
-		display: flex;
-		align-items: center;
-		justify-content: space-between;
-		position: fixed;
-		left: 0;
-		top: 0;
-		/* #ifdef H5 */
-		top: 44px;
-		/* #endif */
-		z-index: 999;
-		transition: opacity .4s;
-	}
-
-	.tui-rolling-search {
-		width: 100%;
-		height: 60rpx;
-		border-radius: 35rpx;
-		padding: 0 40rpx 0 30rpx;
-		box-sizing: border-box;
-		background-color: #fff;
-		display: flex;
-		align-items: center;
-		flex-wrap: nowrap;
-		color: #999;
-	}
-
-	.tui-category {
-		font-size: 24rpx;
-		color: #fff;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		flex-direction: column;
-		margin: 0;
-		margin-right: 22rpx;
-		flex-shrink: 0;
-	}
-
-	.tui-category-scale {
-		transform: scale(0.7);
-		line-height: 24rpx;
-	}
-
-	.tui-icon-category {
-		line-height: 20px !important;
-		margin-bottom: 0 !important;
-	}
-
-	.tui-swiper {
-		font-size: 26rpx;
-		height: 60rpx;
-		flex: 1;
-		padding-left: 12rpx;
-	}
-
-	.tui-swiper-item {
-		display: flex;
-		align-items: center;
-	}
-
-	.tui-hot-item {
-		line-height: 26rpx;
-		white-space: nowrap;
-		overflow: hidden;
-		text-overflow: ellipsis;
-	}
-
-	.tui-header-banner {
-		padding-top: 200rpx;
-		box-sizing: border-box;
-		background: #aaffff;
-	}
-
-	.tui-hot-search {
-		color: #fff;
-		font-size: 24rpx;
-		display: flex;
-		align-items: center;
-		justify-content: space-between;
-		padding: 0 20rpx;
-		box-sizing: border-box;
-		position: relative;
-		z-index: 2;
-	}
-
-	.tui-hot-tag {
-		background-color: rgba(255, 255, 255, 0.15);
-		padding: 10rpx 24rpx;
-		border-radius: 30rpx;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		line-height: 24rpx;
-	}
-
-	.tui-banner-bg {
-		display: flex;
-		height: 180rpx;
-		background-color: #aaffff;
-		position: relative;
-	}
-
-	.tui-primary-bg {
-		width: 50%;
-		display: inline-block;
-		height: 224rpx;
-		border: 1px solid transparent;
-		position: relative;
-		z-index: 1;
-		background-color: #aaffff;
-	}
-
-	.tui-route-left {
-		transform: skewY(8deg);
-	}
-
-	.tui-route-right {
-		transform: skewY(-8deg);
-	}
-
-	.tui-banner-box {
-		width: 100%;
-		padding: 0 20rpx;
-		box-sizing: border-box;
-		position: absolute;
-		/* overflow: hidden; */
-		z-index: 99;
-		bottom: -80rpx;
-		left: 0;
-	}
-
-	.tui-banner-swiper {
-		width: 100%;
-		height: 240rpx;
-		border-radius: 20rpx;
-		overflow: hidden;
-		transform: translateY(0);
-		background-color: #f8f8f8;
-	}
-
-	.tui-slide-image {
-		width: 100%;
-		height: 240rpx;
-		display: block;
-	}
-
-	/* #ifdef MP-WEIXIN */
-	.tui-banner-swiper .wx-swiper-dot {
-		width: 8rpx;
-		height: 8rpx;
-		display: inline-flex;
-		background: none;
-		justify-content: space-between;
-	}
-
-	.tui-banner-swiper .wx-swiper-dot::before {
-		content: '';
-		flex-grow: 1;
-		background-color: rgba(255, 255, 255, 0.8);
-		border-radius: 16rpx;
-		overflow: hidden;
-	}
-
-	.tui-banner-swiper .wx-swiper-dot-active::before {
-		background-color: #fff;
-	}
-
-	.tui-banner-swiper .wx-swiper-dot.wx-swiper-dot-active {
-		width: 16rpx;
-	}
-
-	/* #endif */
-
-	/* #ifndef MP-WEIXIN */
-	>>>.tui-banner-swiper .uni-swiper-dot {
-		width: 8rpx;
-		height: 8rpx;
-		display: inline-flex;
-		background-color: none;
-		justify-content: space-between;
-	}
-
-	>>>.tui-banner-swiper .uni-swiper-dot::before {
-		content: '';
-		flex-grow: 1;
-		background-color: rgba(255, 255, 255, 0.8);
-		border-radius: 16rpx;
-		overflow: hidden;
-	}
-
-	>>>.tui-banner-swiper .uni-swiper-dot-active::before {
-		background-color: #fff;
-	}
-
-	>>>.tui-banner-swiper .uni-swiper-dot.uni-swiper-dot-active {
-		width: 16rpx;
-	}
-
-	/* #endif */
-
-	.tui-product-category {
-		padding: 80rpx 20rpx 30rpx 20rpx;
-		box-sizing: border-box;
-		display: flex;
-		align-items: center;
-		justify-content: space-between;
-		flex-wrap: wrap;
-		font-size: 24rpx;
-		color: #555;
-		/* margin-bottom: 20rpx; */
-	}
-
-	.tui-category-item {
-		width: 20%;
-		height: 118rpx;
-		display: flex;
-		align-items: center;
-		justify-content: space-between;
-		flex-direction: column;
-		padding-top: 30rpx;
-	}
-
-	.tui-category-img {
-		height: 80rpx;
-		width: 80rpx;
-		display: block;
-	}
-
-	.tui-category-name {
-		line-height: 24rpx;
-	}
-
-	.tui-block__box {
-		width: 100%;
-		padding: 0 25rpx 25rpx;
-		box-sizing: border-box;
-		background-color: #ffffff;
-		border-radius: 20rpx;
-		overflow: hidden;
-	}
-
-	.tui-product-box {
-		margin-top: 60rpx;
-		padding: 0 0;
-		box-sizing: border-box;
-	}
-
-	.tui-img__coupon {
-		width: 100%;
-		height: 184rpx;
-		display: block;
-	}
-
-	.tui-mtop__20 {
-		margin-top: 100rpx;
-	}
-
-	.tui-bg-white {
-		background-color: #fff;
-	}
-
-	.tui-group-name {
-		width: 100%;
-		font-size: 34rpx;
-		line-height: 34rpx;
-		font-weight: bold;
+		height: 214px;
+		line-height: 20px;
+		background-color: rgba(186, 231, 255, 0.5);
 		text-align: center;
-		padding: 30rpx 0;
-		display: flex;
-		align-items: center;
-		justify-content: space-between;
-		color: #333;
-	}
-
-	.tui-sub__desc {
-		color: #34c7a9;
-		font-size: 28rpx;
-		font-weight: 400;
-		padding-left: 25rpx;
-	}
-
-	.tui-color__pink {
-		color: #EF1346;
-	}
-
-	.tui-seckill__box {
-		display: flex;
-		align-items: center;
-	}
-
-	.tui-seckill__img {
-		width: 160rpx;
-		height: 34rpx;
-	}
-
-	.tui-countdown__box {
-		width: 228rpx;
-		display: flex;
-		align-items: center;
-
-		color: #fff;
-		background-color: #fff;
-		font-weight: 400;
-		border: 1rpx solid #eb0909;
-		height: 40rpx;
-		border-radius: 30px;
-		overflow: hidden;
-		margin-left: 25rpx;
-	}
-
-	.tui-countdown__title {
-		width: 100rpx;
-		height: 100%;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		background-color: #eb0909;
-		flex-shrink: 0;
-		font-size: 24rpx;
-		line-height: 24rpx;
-	}
-
-	.tui-flex__center {
-		flex: 1;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-	}
-
-	/*秒杀商品*/
-	.tui-goods__list {
-		display: flex;
-		align-items: center;
-	}
-
-	.tui-goods__item {
-		background-color: #fff;
-		width: 150rpx;
-		height: 230rpx;
-		border-radius: 6rpx;
-		box-sizing: border-box;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		flex-direction: column;
-		flex-shrink: 0;
-		margin-right: 18rpx;
-	}
-
-	.tui-goods__imgbox {
-		width: 150rpx;
-		height: 150rpx;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		box-sizing: border-box;
-	}
-
-	.tui-goods__img {
-		max-width: 150rpx;
-		max-height: 150rpx;
-		display: block;
-	}
-
-	.tui-pri__box {
-		max-width: 150rpx;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-	}
-
-	.tui-sale-pri {
-		display: flex;
-		align-items: flex-end;
-		padding: 10rpx 0 8rpx;
-		box-sizing: border-box;
-		font-size: 28rpx;
-		line-height: 28rpx;
-		color: #eb0909;
-		white-space: nowrap;
-		text-overflow: ellipsis;
-		overflow: hidden;
-	}
-
-	.tui-size-sm {
-		font-size: 24rpx;
-		line-height: 24rpx;
-		transform: scale(0.8);
-		transform-origin: 0 50%;
-	}
-
-	.tui-original__pri {
-		font-size: 24rpx;
-		line-height: 24rpx;
-		color: #999999;
-		transform-origin: center 10%;
-		transform: scale(0.8);
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		text-decoration: line-through;
-	}
-
-	/*秒杀商品*/
-
-	.tui-more__box {
-		display: flex;
-		align-items: center;
-		font-weight: 400;
-		color: #999;
-	}
-
-	.tui-more__box text {
-		font-size: 24rpx;
-		line-height: 24rpx;
-	}
-
-	.tui-new-box {
-		display: flex;
-		align-items: center;
-		justify-content: space-between;
-		flex-wrap: wrap;
-		color: #ffffff;
-		
-	}
-
-	.tui-new-item1 {
-		width: 49%;
-		height: 180rpx;
-		padding: 0 20rpx;
-		box-sizing: border-box;
-		display: flex;
-		align-items: center;
-		background: #19e8ff;
-		position: relative;
-		border-radius: 12rpx;
 	}
 	
-	.tui-new-item2 {
-		width: 49%;
-		height: 180rpx;
-		padding: 0 20rpx;
-		box-sizing: border-box;
-		display: flex;
-		align-items: center;
-		background: #55ff7f;
-		position: relative;
-		border-radius: 12rpx;
-	}
-
-	.tui-new-mtop {
-		margin-top: 2%;
-	}
-
-	.tui-title-box {
-		font-size: 24rpx;
-	}
-
-	.tui-new-title {
-		font-size: 26rpx;
-		line-height: 32rpx;
-		word-break: break-all;
-		overflow: hidden;
-		text-overflow: ellipsis;
-		display: -webkit-box;
-		-webkit-box-orient: vertical;
-		-webkit-line-clamp: 2;
-	}
-
-	.tui-new-price {
-		padding-top: 18rpx;
-	}
-
-	.tui-new-present {
-		color: #ff201f;
-		font-weight: bold;
-	}
-
-	.tui-new-original {
-		display: inline-block;
-		color: #a0a0a0;
-		text-decoration: line-through;
-		padding-left: 12rpx;
-		transform: scale(0.8);
-		transform-origin: center center;
-	}
-
-	.tui-new-img {
-		width: 148rpx;
-		height: 148rpx;
-		display: block;
-		flex-shrink: 0;
-	}
-
-	.tui-new-label {
-		width: 56rpx;
-		height: 56rpx;
-		border-top-left-radius: 12rpx;
+	.info{
+		margin-left: 5%;
+		top: 154px;
+		width: 90%;
+		height: 157px;
+		line-height: 20px;
+		border-radius: 10px 10px 10px 10px;
+		background-color: rgba(0, 80, 179, 1);
+		color: rgba(255, 255, 255, 1.0);
+		font-size: 14px;
+		text-align: center;
+		box-shadow: 0px 2px 6px 0px rgba(64, 169, 255, 1);
+		font-family: Arial;
 		position: absolute;
-		left: 0;
-		top: 0;
 	}
-
-	.tui-title__img {
+	
+	.text1{
+		position: absolute;
+		left: 24px;
+		top: 77px;
+		width: 113px;
+		height: 82px;
+		color: rgba(16, 16, 16, 1);
+		font-size: 28px;
+		text-align: left;
+		font-family: PingFangSC-regular;
+	}
+	
+	.text2{
+		position: absolute;
+		left: 24px;
+		top: 117px;
+		width: 273px;
+		height: 20px;
+		color: rgba(16, 16, 16, 1);
+		font-size: 14px;
+		text-align: left;
+		font-family: PingFangSC-regular;
+	}
+	
+	.text3{
+		position: absolute;
+		top: 15px;
 		width: 100%;
-		padding: 30rpx 0;
-		display: flex;
-		justify-content: center;
+		height: 57px;
+		color: rgba(251, 251, 251, 1);
+		font-size: 20px;
+		text-align: center;
+		font-family: PingFangSC-regular;
 	}
-
-	.tui-title__img image {
-		width: 352rpx;
-		height: 32rpx;
+	
+	.text4{
+		position: absolute;
+		left: 25%;
+		top: 45px;
+		width: 85px;
+		height: 41px;
+		color: rgba(251, 251, 251, 1);
+		font-size: 14px;
+		text-align: left;
+		font-family: PingFangSC-regular;
 	}
-
-	.tui-product-list {
-		display: flex;
-		justify-content: space-between;
-		flex-direction: row;
-		flex-wrap: wrap;
-		box-sizing: border-box;
-		/* padding-top: 20rpx; */
+	
+	.text5{
+		position: absolute;
+		top: 15px;
+		width: 100%;
+		height: 57px;
+		color: rgba(251, 251, 251, 1);
+		font-size: 20px;
+		text-align: center;
+		font-family: PingFangSC-regular;
 	}
-
-	.tui-product-container {
+	
+	.product-box1{
+		position: absolute;
+		left: 16px;
+		top: 325px;
+		width: 166px;
+		height: 101px;
+		line-height: 20px;
+		border-radius: 10px;
+		background-color: rgba(105, 192, 255, 1);
+		text-align: center;
+		box-shadow: 0px 2px 6px 0px rgba(145, 213, 255, 1);
+	}
+	
+	.product-box2{
+		position: absolute;
+		left: 195px;
+		top: 326px;
+		width: 166px;
+		height: 101px;
+		line-height: 20px;
+		border-radius: 10px;
+		background-color: rgba(149, 222, 100, 1);
+		text-align: center;
+		box-shadow: 0px 2px 6px 0px rgba(217, 247, 190, 1);
+	}
+	
+	.show-text{
+		position: absolute;
+		left: 137px;
+		top: 442px;
+		width: 102px;
+		height: 34px;
+		line-height: 23px;
+		border-radius: 10px 10px 10px 10px;
+		background-color: rgba(154, 154, 154, 1);
+		color: rgba(255, 255, 255, 1);
+		font-size: 16px;
+		text-align: center;
+		font-family: Arial;
+	}
+	
+	.stats1{
+		position: absolute;
+		left: 18px;
+		top: 490px;
+		width: 343px;
+		height: 248px;
+		line-height: 20px;
+		border-radius: 5px;
+		background-color: rgba(255, 255, 255, 1);
+		text-align: center;
+		border: 1px solid rgba(221, 219, 219, 1);
+	}
+	
+	.stats2{
+		position: absolute;
+		left: 18px;
+		top: 752px;
+		width: 343px;
+		height: 248px;
+		line-height: 20px;
+		border-radius: 5px;
+		background-color: rgba(255, 255, 255, 1);
+		text-align: center;
+		border: 1px solid rgba(221, 219, 219, 1);
+	}
+	
+	.info-text{
+		position: absolute;
+		top: 20px;
+	}
+	
+	.container{
 		flex: 1;
-		margin-right: 2%;
-	}
-
-	.tui-product-container:last-child {
-		margin-right: 0;
-	}
-
-	.tui-pro-item {
-		width: 100%;
-		margin-bottom: 4%;
-		background: #fff;
-		box-sizing: border-box;
-		border-radius: 12rpx;
-		overflow: hidden;
-	}
-
-	.tui-pro-img {
-		width: 100%;
-		display: block;
-	}
-
-	.tui-pro-content {
-		display: flex;
-		flex-direction: column;
-		justify-content: space-between;
-		box-sizing: border-box;
-		padding: 20rpx;
-	}
-
-	.tui-pro-tit {
-		color: #2e2e2e;
-		font-size: 26rpx;
-		line-height: 32rpx;
-		word-break: break-all;
-		overflow: hidden;
-		text-overflow: ellipsis;
-		display: -webkit-box;
-		-webkit-box-orient: vertical;
-		-webkit-line-clamp: 2;
-	}
-
-	.tui-pro-price {
-		padding-top: 18rpx;
-	}
-
-	.tui-sale-price {
-		font-size: 34rpx;
-		font-weight: 500;
-		color: #e41f19;
-	}
-
-	.tui-factory-price {
-		font-size: 24rpx;
-		color: #a0a0a0;
-		text-decoration: line-through;
-		padding-left: 12rpx;
-	}
-
-	.tui-pro-pay {
-		padding-top: 10rpx;
-		font-size: 24rpx;
-		color: #656565;
 	}
 </style>
